8fc5ace7efed2639e1d93f00fdbd05d577d14081
Rhama Krisner <66706928+rhama-krisner@users.noreply.github.com>
2021-06-16 15:22:49 -0300

    Confogurações

--- setup/settings.py
+++ setup/settings.py
@@ -28,9 +28,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
+# Application definition 
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
