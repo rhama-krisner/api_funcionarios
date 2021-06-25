72259c5f349512de31d8ee516423fcf5e3a0fb04
Rhama Krisner <66706928+rhama-krisner@users.noreply.github.com>
2021-06-25 20:44:44 -0300

    Update 06.25

--- funcionarios/models.py
+++ funcionarios/models.py
@@ -3,18 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Funcionarios(models.Model):
-    genero = (
-        (u'H', u'Homem'),
-        (u'M', u'Mulher'),
-        (u'N', u'Não Binário'),
-    )
+    
 
-    nome = models.CharField(max_length=100)
-    genero = models.CharField(max_length=3, choices=genero)
-    cpf = models.CharField(max_length=11)
-    cargo = models.CharField(max_length=20)
-    salario = models.DecimalField(max_digits=6, decimal_places=2)
-    data_nascimento = models.DateField()
+    nome = models.CharField(max_length=255)
+    genero = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=255)
+    cargo = models.CharField(max_length=255)
+    salario = models.DecimalField(max_digits=255)
 
     def __str__(self):
         return self.nome
--- funcionarios/serializer.py
+++ funcionarios/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import Funcionarios
+from funcionarios.models import Funcionarios
 
 
 class Funcionarios_Serializer(serializers.ModelSerializer):
